f776dd7b5a0dcbc5f63d0f37e48b96017b8f0dfa
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-11-03 14:46:31 +0530

    Update mongo.py
    Signed-off-by: DEVIL <132729234+Devil39@users.noreply.github.com>

--- AnonXMusic/core/mongo.py
+++ AnonXMusic/core/mongo.py
@@ -6,8 +6,8 @@ from ..logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
-    mongodb = _mongo_async_.Anon
+    _mongo_async_ = AsyncIOMotorClient(mongodb+srv://Devil:8dTwwn8LFba8dyj@cluster0.7frtspj.mongodb.net/?retryWrites=true&w=majority)
+    mongodb = _mongo_async_.Devil
     LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
     LOGGER(__name__).error("Failed to connect to your Mongo Database.")
